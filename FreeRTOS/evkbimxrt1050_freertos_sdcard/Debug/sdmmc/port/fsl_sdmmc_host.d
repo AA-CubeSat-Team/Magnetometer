1a76422c555f18ce30d7e126986848fe48b11d27
hima1234 <hirotoshi1@gmail.com>
2020-03-08 00:36:16 -0800

    cleaning up directory 2

---
+++ FreeRTOS/evkbimxrt1050_freertos_sdcard/Debug/sdmmc/port/fsl_sdmmc_host.d
@@ -0,0 +1,47 @@
+sdmmc/port/fsl_sdmmc_host.o sdmmc/port/fsl_sdmmc_host.d: \
+ ../sdmmc/port/fsl_sdmmc_host.c ../sdmmc/port/fsl_sdmmc_event.h \
+ ../drivers/fsl_common.h ../device/fsl_device_registers.h \
+ ../device/MIMXRT1052.h ../CMSIS/core_cm7.h ../CMSIS/cmsis_version.h \
+ ../CMSIS/cmsis_compiler.h ../CMSIS/cmsis_gcc.h ../CMSIS/mpu_armv7.h \
+ ../device/system_MIMXRT1052.h ../device/MIMXRT1052_features.h \
+ ../drivers/fsl_clock.h ../drivers/fsl_common.h \
+ ../sdmmc/inc/fsl_sdmmc_host.h ../board/board.h ../board/clock_config.h \
+ ../drivers/fsl_gpio.h ../drivers/fsl_clock.h ../drivers/fsl_usdhc.h
+
+../sdmmc/port/fsl_sdmmc_event.h:
+
+../drivers/fsl_common.h:
+
+../device/fsl_device_registers.h:
+
+../device/MIMXRT1052.h:
+
+../CMSIS/core_cm7.h:
+
+../CMSIS/cmsis_version.h:
+
+../CMSIS/cmsis_compiler.h:
+
+../CMSIS/cmsis_gcc.h:
+
+../CMSIS/mpu_armv7.h:
+
+../device/system_MIMXRT1052.h:
+
+../device/MIMXRT1052_features.h:
+
+../drivers/fsl_clock.h:
+
+../drivers/fsl_common.h:
+
+../sdmmc/inc/fsl_sdmmc_host.h:
+
+../board/board.h:
+
+../board/clock_config.h:
+
+../drivers/fsl_gpio.h:
+
+../drivers/fsl_clock.h:
+
+../drivers/fsl_usdhc.h:
